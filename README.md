d819153db81186379fecfb65a1ac0bc075a1e63b
Shyrshling <dj.gleb@list.ru>
2020-03-07 07:30:34 +0000

    Initial commit.

---
+++ README.md
@@ -0,0 +1,2 @@
+# 🐱‍🐉 wvraptor-fronted
+This project uses Sapper as main framework for server-rendered application.
---
+++ src/routes/index.svelte
@@ -0,0 +1,8 @@
+<div class="bg-red-500 w-full h-full flex justify-center items-center text-center">
+  <div>
+    <p class="text-xl">
+      Hello there!
+    </p>
+    <p class="text-sm">Work In Progress.</p>
+  </div>
+</div>
